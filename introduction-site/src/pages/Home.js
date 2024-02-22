3a82a66f7afa345592a52d4a86b2d16b6ae1373f
Lindsay Abdou <abdoulindsay@gmail.com>
2024-02-21 21:32:30 -0800

    router finished

--- introduction-site/src/pages/Home.js
+++ introduction-site/src/pages/Home.js
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
 import '../App';
+import './Piano';
 
 function Home() {
     return (
         <div>
+            <header>
+            <Link to="/piano">Piano</Link>
+            </header>
             <h1>
                New Site Coming Soon! 
             </h1>
